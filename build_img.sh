9eaa3f80f7b2e211f5b81fb0ed83629605288cc4
ccyd <hawnagyudong@outlook.com>
2023-05-28 02:43:29 +0800

    fix: 修改了syscall read和write，使用正确的接口保证可以跨cluster读写；以后外部只调用axfs的api接口，所需要的接口先封装到api中

--- build_img.sh
+++ build_img.sh
@@ -1,10 +1,10 @@
 rm sdcard.img
-dd if=/dev/zero of=sdcard.img bs=3M count=1024
-mkfs.vfat -F 32 sdcard.img
+dd if=/dev/zero of=sdcard.img bs=1M count=128
+mkfs.vfat -F 32 -S 512 sdcard.img
 mkdir -p mnt
 sudo mount sdcard.img mnt
 # 此处生成的是libc的测例
-sudo cp -r ./testcases/oscomp/* ./mnt/
+sudo cp -r ./testcases/open ./mnt/
 sudo umount mnt
 rm -rf mnt
 sudo chmod 777 sdcard.img
